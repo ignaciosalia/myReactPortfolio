8450b55a4975bb099ae16fa7822b09726f7950c9
ignaciosalia <ignaciosalia@gmail.com>
2021-04-07 21:40:28 -0500

    added font awesome icons to contact area

--- src/Contacto.jsx
+++ src/Contacto.jsx
@@ -12,10 +12,10 @@ export default class Contacto extends React.Component{
                 <h2 className="subtitulo">Aquí me encuentras</h2>
                 <Container >
                     <Row>
-                        <Col className="rowContactMember"> <Link to="/Experiencia">Experiencia</Link> </Col>
-                        <Col className="rowContactMember"> <a href="https://www.facebook.com/JislCorp-293709961299868">Facebook</a> </Col>
-                        <Col className="rowContactMember"> <a href="https://twitter.com/IgnacioSalia">Twitter</a> </Col>
-                        <Col className="rowContactMember"> <a href="https://www.linkedin.com/in/jorge-ignacio-salia-982686162/">Linkedin</a></Col>
+                        <Col className="rowContactMember"> <Link to="/Experiencia"> <i className="fa fa-address-card"></i> </Link> </Col>
+                        <Col className="rowContactMember"> <a href="https://www.facebook.com/JislCorp-293709961299868"> <i className="fa fa-facebook"> </i></a> </Col>
+                        <Col className="rowContactMember"> <a href="https://twitter.com/IgnacioSalia"><i className="fa fa-twitter"> </i></a> </Col>
+                        <Col className="rowContactMember"> <a href="https://www.linkedin.com/in/jorge-ignacio-salia-982686162/"><i className="fa fa-linkedin"> </i></a></Col>
                     </Row>
                 </Container>
             </div>
