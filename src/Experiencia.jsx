3672b043701e6afd8f1c2c4d001b013b35d9fea7
ignaciosalia <ignaciosalia@gmail.com>
2021-04-09 15:10:16 -0500

    Improve in landing page and expirience

--- src/Experiencia.jsx
+++ src/Experiencia.jsx
@@ -9,20 +9,6 @@ export default class Experiencia extends React.Component{
         return(
             <CardGroup>
                 <Card style={{ width: '18rem', margin:'2em'}}>
-                    <Card.Img variant="top" src={"https://unamglobal.unam.mx/wp-content/uploads/2017/05/LOGO-UNAM.png"} />
-                    <Card.Body>
-                        <Card.Title className="cardtitle">
-                            <Link to="" > UNAM FES Acatlan</Link>
-                        </Card.Title>
-                        <Card.Text className="cardtext">
-                            Mi paso por la mejor universidad de México:
-                        </Card.Text>
-                        <Card.Footer className="cardfooter">
-                            Me tomo mucho tiempo llegar hasta ahí pero no cambiaría mi viaje por nada, da click en el link para saber más...
-                        </Card.Footer>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem', margin:'2em'}}>
                     <Card.Img variant="top" src={"https://pbs.twimg.com/profile_images/1380321175214071808/Arp6kg7Q_400x400.jpg"} />
                     <Card.Body>
                         <Card.Title className="cardtitle">
@@ -33,6 +19,20 @@ export default class Experiencia extends React.Component{
                         </Card.Text>
                         <Card.Footer className="cardfooter">
                             En lo personal soy enfocado y dedicado, soy puntal y en el link puedes conocer más lo que hago...
+                        </Card.Footer>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem', margin:'2em'}}>
+                    <Card.Img variant="top" src={"https://unamglobal.unam.mx/wp-content/uploads/2017/05/LOGO-UNAM.png"} />
+                    <Card.Body>
+                        <Card.Title className="cardtitle">
+                            <Link to="" > UNAM FES Acatlan</Link>
+                        </Card.Title>
+                        <Card.Text className="cardtext">
+                            Mi paso por la mejor universidad de México:
+                        </Card.Text>
+                        <Card.Footer className="cardfooter">
+                            Me tomo mucho tiempo llegar hasta ahí pero no cambiaría mi viaje por nada, da click en el link para saber más...
                         </Card.Footer>
                     </Card.Body>
                 </Card>
--- src/Presentacion.jsx
+++ src/Presentacion.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Contacto from './Contacto'
 import ServicesResume from './ServicesResume'
+import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom'
 
 export default class Presentacion extends React.Component{
     render(){
@@ -10,6 +12,20 @@ export default class Presentacion extends React.Component{
                     <h1 className="yomero">Ignacio Salia</h1>
                     <p className="queChingaosHago">Creo sitios Web para Pymes, Frelancer</p>
                 </div>
+                <Card style={{ width: '18rem', margin:'2em'}}>
+                    <Card.Img variant="top" src={"https://pbs.twimg.com/profile_images/1380321175214071808/Arp6kg7Q_400x400.jpg"} />
+                    <Card.Body>
+                        <Card.Title className="cardtitle">
+                            <Link to="" > Mi carrera Profesional</Link>
+                        </Card.Title>
+                        <Card.Text className="cardtext">
+                            Comencé mi carrera profesional y no he parado...
+                        </Card.Text>
+                        <Card.Footer className="cardfooter">
+                            En lo personal soy enfocado y dedicado, soy puntal y en el link puedes conocer más lo que hago...
+                        </Card.Footer>
+                    </Card.Body>
+                </Card>
                 <div className="mycontact">
                     <Contacto></Contacto>
                 </div>
