04dcf1dc3c4ab79be1888495170fe3ff3b70a872
ignaciosalia <ignaciosalia@gmail.com>
2021-04-08 19:38:49 -0500

    Changes to the experiencia part now looks pretty

--- src/Experiencia.jsx
+++ src/Experiencia.jsx
@@ -1,19 +1,50 @@
 
 import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import { Link } from 'react-router-dom'
 
 export default class Experiencia extends React.Component{
     render(){
         return(
+            <>
+            <CardGroup>
+                <Card style={{ width: '18rem', margin:'2em'}}>
+                    <Card.Img variant="top" src={"https://unamglobal.unam.mx/wp-content/uploads/2017/05/LOGO-UNAM.png"} />
+                    <Card.Body>
+                        <Card.Title className="cardtitle">
+                            <Link to="" > UNAM FES Acatlan</Link>
+                        </Card.Title>
+                        <Card.Text className="cardtext">
+                            Mi paso por la mejor universidad de México:
+                        </Card.Text>
+                        <Card.Footer className="cardfooter">
+                            Me tomo mucho tiempo llegar hasta ahí pero no cambiaría mi viaje por nada, da click en el link para saber más...
+                        </Card.Footer>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem', margin:'2em'}}>
+                    <Card.Img variant="top" src={"https://unamglobal.unam.mx/wp-content/uploads/2017/05/LOGO-UNAM.png"} />
+                    <Card.Body>
+                        <Card.Title className="cardtitle">
+                            <Link to="" > Mi carrera Profesional</Link>
+                        </Card.Title>
+                        <Card.Text className="cardtext">
+                            Comencé mi carrera profesional y no he parado...
+                        </Card.Text>
+                        <Card.Footer className="cardfooter">
+                            En lo personal soy enfocado y dedicado, soy puntal y en el link puedes conocer más lo que hago...
+                        </Card.Footer>
+                    </Card.Body>
+                </Card>
+            </CardGroup>
             <div className="experiencia">
-                    <div className="educacion">
-                        <a href=""><h2>UNAM</h2></a>
-                        <h3>La historia de mi paso por la universidad</h3>
-                    </div>
                     <div className="community-manager">
                         <a href=""><h2>CV</h2></a>
                         <h3>Experiencia laboral previa</h3>
                     </div>
             </div>
+            </>
         );
     }
 }
