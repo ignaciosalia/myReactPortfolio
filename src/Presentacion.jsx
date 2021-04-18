ab9ad00a32488b13b5ba1b3579bd27a119b97dc7
ignaciosalia <ignaciosalia@gmail.com>
2021-04-17 21:14:25 -0500

    Desktop presentation allmost ready

--- src/Presentacion.jsx
+++ src/Presentacion.jsx
@@ -1,36 +1,47 @@
 import React, { Component } from 'react'
 import Contacto from './Contacto'
+import Clientes from './Clientes'
 import ServicesResume from './ServicesResume'
 import Card from 'react-bootstrap/Card'
 import { Link } from 'react-router-dom'
-
+import Container  from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/col'
 
 export default class Presentacion extends React.Component{
     render(){
         return(
             <div className="presentacion">
-                <div className="myself">
-                    <h1 className="yomero">Ignacio Salia</h1>
-                    <p className="queChingaosHago">Creo sitios Web para Pymes, Frelancer</p>
-                </div>
-                <Card style={{ width: '18em', margin:'2em'}}>
-                    <Card.Img variant="top" src={"https://pbs.twimg.com/profile_images/1380321175214071808/Arp6kg7Q_400x400.jpg"} />
-                    <Card.Body>
-                        <Card.Title className="cardtitle">
-                            <Link to="/Carrera"> Mi carrera Profesional</Link>
-                        </Card.Title>
-                        <Card.Text className="cardtext">
-                            Comencé mi carrera profesional y no he parado...
-                        </Card.Text>
-                        <Card.Footer className="cardfooter">
-                            En lo personal soy enfocado y dedicado, soy puntal y en el link puedes conocer más lo que hago...
-                        </Card.Footer>
-                    </Card.Body>
-                </Card>
+                <Container>
+                    <Row>
+                        <Col>
+                        <div className="myself">
+                            <h1 className="yomero">Ignacio Salia</h1>
+                            <p className="queChingaosHago">Creo sitios Web que atraen clientes</p>
+                        </div>
+                        </Col>
+                        <Col>
+                        <Card >
+                            <Card.Img  variant="top" src={"https://pbs.twimg.com/profile_images/1380321175214071808/Arp6kg7Q_400x400.jpg"} />
+                            <Card.Body >
+                                <Card.Title className="cardtitle">
+                                    <Link to="/Carrera"> Mi carrera Profesional</Link>
+                                </Card.Title>
+                                <Card.Text className="cardtext">
+                                    Comencé mi carrera profesional y no he parado...
+                                </Card.Text>
+                                <Card.Footer className="cardfooter">
+                                    En lo personal soy enfocado y dedicado, soy puntal y en el link puedes conocer más lo que hago...
+                                </Card.Footer>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+                    </Row>
                 <div className="mycontact">
                     <Contacto></Contacto>
                 </div>
-                <ServicesResume></ServicesResume>
+                <Clientes></Clientes>
+                </Container>
             </div>
         );
     }
