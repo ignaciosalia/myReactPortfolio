e9f75c64f5095c2fe67862359216862e3d12f641
ignaciosalia <ignaciosalia@gmail.com>
2021-05-17 17:47:59 -0500

    Updates

---
+++ precache-manifest.42fa2a99b7fa2f48f3ab440a5e187d6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2388725349da81869433",
+    "url": "/myReactPortfolio/static/css/main.9801c97c.chunk.css"
+  },
+  {
+    "revision": "2388725349da81869433",
+    "url": "/myReactPortfolio/static/js/main.58ce8ba9.chunk.js"
+  },
+  {
+    "revision": "066ef05be68688ff3712",
+    "url": "/myReactPortfolio/static/js/runtime~main.08e801d8.js"
+  },
+  {
+    "revision": "ace3a0a945d5b2fd6bd5",
+    "url": "/myReactPortfolio/static/css/2.605b0d72.chunk.css"
+  },
+  {
+    "revision": "ace3a0a945d5b2fd6bd5",
+    "url": "/myReactPortfolio/static/js/2.8f0ddee5.chunk.js"
+  },
+  {
+    "revision": "54086304e56ca0e51fcf",
+    "url": "/myReactPortfolio/static/js/3.9c6d353f.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/myReactPortfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "53c6276476663df2e8ae94f11af29672",
+    "url": "/myReactPortfolio/index.html"
+  }
+];
